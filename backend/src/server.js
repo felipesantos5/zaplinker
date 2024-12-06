163d67bbae8ff72aaa58501321caec455cef9020
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-12-06 00:37:55 -0300

    add: route 0.0.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -534,6 +534,6 @@ app.get("/api/workspace/:id/qrcode", authMiddleware, async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
