63ae51985ac11ca086b9485e70e188e84e9b25e0
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-12-09 23:56:45 -0300

    style: change spinner color

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export const Spinner = ({ size = 10 }) => (
   <div className="flex justify-center items-center">
     <div
-      className={`spinner w-${size} h-${size} dark:border-white dark:border-t-4 dark:border-t-zinc-400`}
+      className={`spinner w-${size} h-${size} dark:border-white dark:border-t-4 dark:border-t-zinc-300`}
       style={{
         width: `${size * 4}px`,
         height: `${size * 4}px`
