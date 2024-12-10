5b1c7c841ab8f5bfd8e1ed2eb9787cb144f0dd31
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-12-09 22:33:56 -0300

    test: test spnier dakmode color

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export const Spinner = ({ size = 10 }) => (
   <div className="flex justify-center items-center">
     <div
-      className={`spinner w-${size} h-${size}`}
+      className={`spinner w-${size} h-${size} dark:border-white`}
       style={{
         width: `${size * 4}px`,
         height: `${size * 4}px`
